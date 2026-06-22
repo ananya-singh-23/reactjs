2135359d996b2c9a872144ebaf0bed94f241e35b
ananya-singh-23 <ananyasingh8763@gmail.com>
2026-06-22 19:47:38 +0530

    project-1 done

---
+++ H1making.jsx
@@ -0,0 +1,11 @@
+let h1 =<h1> hello i am here!! </h1>
+
+function fun1() {
+    console.log("Are you there?");    
+}
+
+//fun1()  1st way of caling function
+
+//we can alos call by:
+
+<fun1 /> 
---
+++ project-1/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 id='parent' >Welcome to my React App!</h1>
+      <p id='child1'>This is a simple React app.</p>
+      <p id='child2'>Enjoy exploring React!</p>
+    </>
+  )
+}
+
+export default App
